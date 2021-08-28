9646cb4ce308adf7d21761d60daa64272dbffd9d
Pedro Resende Breitschaft <pedro_brei@hotmail.com>
2021-08-28 12:44:47 -0300

    Controllers created

---
+++ app/helpers/message_infos_helper.rb
@@ -0,0 +1,2 @@
+module MessageInfosHelper
+end
---
+++ test/controllers/message_infos_controller_test.rb
@@ -0,0 +1,7 @@
+require "test_helper"
+
+class MessageInfosControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
